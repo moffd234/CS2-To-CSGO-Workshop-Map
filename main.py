7dd2b4df10940399597defa9f155ec0086232a79
Daniel Moffett <moffd234@gmail.com>
2026-03-11 05:13:04 -0400

    Moved functions in order of their calls

--- main.py
+++ main.py
@@ -4,9 +4,33 @@ import time
 from argparse import ArgumentParser, Namespace
 from zipfile import ZipFile
 
-STEAM_APPS_DIRECTORY = r"C:\Program Files (x86)\Steam\steamapps"
-DEFAULT_CS2_DIRECTORY = STEAM_APPS_DIRECTORY + r"\workshop\content\730"
-DEFAULT_CSGO_DIRECTORY = r"\common\csgo legacy\csgo\maps"
+STEAM_APPS_DIRECTORY = r""
+DEFAULT_CS2_DIRECTORY = r"C:\Program Files (x86)\Steam\steamapps\workshop\content\730"
+DEFAULT_CSGO_DIRECTORY = r"C:\Program Files (x86)\Steam\steamapps\common\csgo legacy\csgo\maps"
+
+
+def unzip_and_move(zip_path: str, new_directory: str) -> None:
+    with ZipFile(zip_path, "r") as zObject:
+        zObject.extractall(new_directory)
+        os.remove(zip_path)
+
+
+def rename_files_and_move(parent_directory: str, dst_directory: str) -> None:
+    for folder in os.listdir(parent_directory):
+        full_folder: str = os.path.join(parent_directory, folder)
+
+        if not os.path.isdir(full_folder):
+            continue
+
+        for file in os.listdir(full_folder):
+
+            if file.endswith(".bin"):
+                src_file_path: str = os.path.join(full_folder, file)
+                zip_name: str = file.replace(".bin", ".zip")
+                zip_path: str = os.path.join(full_folder, zip_name)
+
+                shutil.copy(src_file_path, zip_path)
+                unzip_and_move(zip_path, dst_directory)
 
 
 def main() -> None:
@@ -15,7 +39,8 @@ def main() -> None:
 
     parser: ArgumentParser = ArgumentParser(description="Flag parser")
     parser.add_argument("-s", "--src", help="Source folder (CS2 folder)", required=False, default=DEFAULT_CS2_DIRECTORY)
-    parser.add_argument("-d", "--dst", help="Destination folder (CSGO folder)", required=False, default=DEFAULT_CSGO_DIRECTORY)
+    parser.add_argument("-d", "--dst", help="Destination folder (CSGO folder)", required=False,
+                        default=DEFAULT_CSGO_DIRECTORY)
 
     arguments: Namespace = parser.parse_args()
 
@@ -42,27 +67,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
-
-def rename_files_and_move(parent_directory: str, dst_directory: str) -> None:
-    for folder in os.listdir(parent_directory):
-        full_folder: str = os.path.join(parent_directory, folder)
-
-        if not os.path.isdir(full_folder):
-            continue
-
-        for file in os.listdir(full_folder):
-
-            if file.endswith(".bin"):
-                src_file_path: str = os.path.join(full_folder, file)
-                zip_name: str = file.replace(".bin", ".zip")
-                zip_path: str = os.path.join(full_folder, zip_name)
-
-                shutil.copy(src_file_path, zip_path)
-                unzip_and_move(zip_path, dst_directory)
-
-
-def unzip_and_move(zip_path: str, new_directory: str) -> None:
-    with ZipFile(zip_path, "r") as zObject:
-        zObject.extractall(new_directory)
-        os.remove(zip_path)
