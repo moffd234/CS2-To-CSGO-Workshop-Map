30975290bee73fb67074e5e2253afa9aacf5c6f0
Daniel Moffett <moffd234@gmail.com>
2026-03-11 04:04:24 -0400

    Wrote rename_files_and_move

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 from zipfile import ZipFile
 
 
@@ -8,6 +9,24 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
+
+def rename_files_and_move(parent_directory: str, new_directory: str) -> None:
+    for folder in os.listdir(parent_directory):
+        full_folder: str = os.path.join(parent_directory, folder)
+
+        if not os.path.isdir(full_folder):
+            continue
+
+        for file in os.listdir(full_folder):
+
+            if file.endswith(".bin"):
+                src_file_path: str = os.path.join(full_folder, file)
+                zip_name: str = file.replace(".bin", ".zip")
+                zip_path: str = os.path.join(full_folder, zip_name)
+
+                shutil.copy(src_file_path, zip_path)
+                unzip_and_move(zip_path, new_directory)
 
 
 def unzip_and_move(zip_path: str, new_directory: str) -> None:
